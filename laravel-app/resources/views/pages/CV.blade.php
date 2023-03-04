d66a0981b394737e74c142d637c04c3c5551797d
lesha13 <leshamashuna@gmail.com>
2023-03-04 12:12:03 +0300

    lab1 added cv

---
+++ laravel-app/resources/views/pages/CV.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+</head>
+<body>
+
+<h1>Milchenko Oleksii</h1>
+<h2>Junior Laravel Developer</h2>
+
+<h3>Skills</h3>
+    <p>PHP, Laravel, HTML, CSS, Javascript, Vue.js, SQL</p>
+<h3>Experience</h3>
+    <ul>
+        <li>Online shop project</li>
+        <li>Laravel CV route app</li>
+    </ul>
+<h3>Education</h3>
+    <p>Uzhhorod National University 2021-2025</p>
+    <p>Bachelors of Computer Science</p>
+</body>
+</html>
--- laravel-app/routes/web.php
+++ laravel-app/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CVController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +17,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/Miliuchenko/cv', [CVController::class, 'cv']);
